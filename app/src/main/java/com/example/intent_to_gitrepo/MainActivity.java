162f23450c5fabed8a10b0e4e6de032e5caead4b
Abu Huraira <iamhuraira429@gmail.com>
2022-05-19 02:52:11 +0500

    write intent code

--- app/src/main/java/com/example/intent_to_gitrepo/MainActivity.java
+++ app/src/main/java/com/example/intent_to_gitrepo/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.intent_to_gitrepo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void clickfunction(View view) {
+        String url = "http://www.example.com";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
